ec7381b4208be88b2c1ca77b44bcd03cd0470811
evelynhuangyt <evelynhuangyt@163.com>
2017-03-20 22:36:31 -0400

    first commit

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var restRouter = require("./routes/rest");
 var indexRouter = require("./routes/index");
 
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://yutong:123@ds125060.mlab.com:25060/personalwebsite");
+mongoose.connect("DATABASE");
 
 app.use("/", indexRouter);
 app.use("/contact/", restRouter);
 app.use("/public", express.static(__dirname + "/public"));
 
 app.listen(3000);
-
